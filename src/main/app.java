8277c07dbf4d92b3fb3a2b744e7a196fbc28838f
molnarbea <molnarbeatrix11@gmail.com>
2026-09-17 15:08:42 +0200

    kever megirva

--- src/main/app.java
+++ src/main/app.java
@@ -1,8 +1,11 @@
 package main;
 
+import java.util.Scanner;
+
 public class app {
     
     static String[] pakli = new String[22];
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         program();
@@ -11,27 +14,60 @@ public class app {
     public static void program() {
         feltolt();
         for (int i = 0; i < 3; i++) {
-            kiark();//1tömb
-            melyik();//scanner
-            kever();//középre
+            kirak();//1tömb
+            int oszlop = melyik();//scannerkever(oszlop);//középre
+            kever(oszlop);
         }
         ezVolt();//11. elem
     }
 
-    private static void kiark() {
-        
+    private static void kirak() {
+        System.out.printf("%-12s %-12s %-12s%n%n","1. oszlop","2. oszlop","3. oszlop");
+        for (int i = 0; i < 7; i++) {
+            System.out.printf("%-12s %-12s %-12s%n", pakli[i*3+1],pakli[i*3+2],pakli[i*3+3]);
+        }
     }
 
     private static void melyik() {
-        
+        System.out.print("\nMelyik oszlop (1-3): ");
+        int oszlop = sc.nextInt();
+        while(oszlop < 1 || oszlop > 3){
+            System.out.println("Hibás oszlop!");
+            System.out.print("Melyik oszlop (1-3): ");
+            oszlop = sc.nextInt();
+        }
+        return oszlop;
     }
 
-    private static void kever() {
-        
+    private static String[] kever(int oszlop) {
+        static String[] regiPakli = pakli.clone();
+        switch (oszlop) {
+            case 1:
+                for (int i = 1; i < 8; i++) {
+                    pakli[i] = regiPakli[20-(i-1)*3];
+                    pakli[i+7] = regiPakli[19-(i-1)*3];
+                    pakli[i+14] = regiPakli[21-(i-1)*3];
+                    
+                }   break;
+            case 2:
+                for (int i = 1; i < 8; i++) {
+                    pakli[i] = regiPakli[19-(i-1)*3];
+                    pakli[i+7] = regiPakli[20-(i-1)*3];
+                    pakli[i+14] = regiPakli[21-(i-1)*3];
+                    
+                }   break;
+            default:
+                for (int i = 1; i < 8; i++) {
+                    pakli[i] = regiPakli[19-(i-1)*3];
+                    pakli[i+7] = regiPakli[21-(i-1)*3];
+                    pakli[i+14] = regiPakli[20-(i-1)*3];
+                    
+                }   break;
+        }
     }
 
     private static void ezVolt() {
-        
+        System.out.println("A választott kártya: "+kevert[11]);
     }
 
     private static void feltolt() {
@@ -43,9 +79,7 @@ public class app {
                     if (i<21) {
                         i++;
                         pakli[i]=szinek[j]+"_"+ertekek[k];
-                        System.out.println(pakli[i]);
                     }
-                    
                 }
             }
         }
